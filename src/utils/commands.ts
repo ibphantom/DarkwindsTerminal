08d3cc10ee9c85168440c7ee41098bb6ecab1164
Zach <62777429+ibphantom@users.noreply.github.com>
2024-05-17 17:06:14 -0400

    Update commands.ts

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -4,129 +4,116 @@ import { history } from '../stores/history';
 import { theme } from '../stores/theme';
 
 const hostname = window.location.hostname;
-let isAuthenticated = false;
 
 export const commands: Record<string, (args: string[]) => Promise<string> | string> = {
- help: () => 'Available commands: ' + Object.keys(commands).join(', '),
- hostname: () => isAuthenticated ? hostname : 'Authentication required',
- whoami: () => isAuthenticated ? 'guest' : 'Authentication required',
- date: () => isAuthenticated ? new Date().toLocaleString() : 'Authentication required',
- vi: () => isAuthenticated ? `why use vi? try 'emacs'` : 'Authentication required',
- vim: () => isAuthenticated ? `why use vim? try 'emacs'` : 'Authentication required',
- emacs: () => isAuthenticated ? `why use emacs? try 'vim'` : 'Authentication required',
- echo: (args: string[]) => isAuthenticated ? args.join(' ') : 'Authentication required',
- sudo: async (args: string[]) => {
-   // Simulate password prompt
-   const enteredPassword = prompt('Enter your password:');
+  help: () => 'Available commands: ' + Object.keys(commands).join(', '),
+  hostname: () => hostname,
+  whoami: () => 'guest',
+  date: () => new Date().toLocaleString(),
+  vi: () => `why use vi? try 'emacs'`,
+  vim: () => `why use vim? try 'emacs'`,
+  emacs: () => `why use emacs? try 'vim'`,
+  echo: (args: string[]) => args.join(' '),
+  sudo: (args: string[]) => {
+    window.open('#');
 
-   // Replace the condition with your actual password validation logic
-   if (enteredPassword === 'DoubleDown!!') {
-       isAuthenticated = true;
-       // Change whoami from guest to sudo
-       commands['whoami'] = () => isAuthenticated ? 'sudo' : 'Authentication required';
-       return `Authentication successful. You can now run '${args.join(' ')}' as root.`;
-   } else {
-       isAuthenticated = false;
-       return 'Authentication failed. Permission denied.';
-   }
-},
+    return `Permission denied: unable to run the command '${args[0]}' as root.`;
+  },
+  theme: (args: string[]) => {
+    const usage = `Usage: theme [args].
+    [args]:
+      ls: list all available themes
+      set: set theme to [theme]
 
- theme: (args: string[]) => {
-   const usage = `Usage: theme [args].
-   [args]:
-     ls: list all available themes
-     set: set theme to [theme]
+    [Examples]:
+      theme ls
+      theme set gruvboxdark
+    `;
+    if (args.length === 0) {
+      return usage;
+    }
 
-   [Examples]:
-     theme ls
-     theme set gruvboxdark
-   `;
-   if (args.length === 0) {
-     return usage;
-   }
+    switch (args[0]) {
+      case 'ls': {
+        let result = themes.map((t) => t.name.toLowerCase()).join(', ');
+        result += `You can preview all these themes here: ${packageJson.repository.url}/tree/master/docs/themes`;
 
-   switch (args[0]) {
-     case 'ls': {
-       let result = themes.map((t) => t.name.toLowerCase()).join(', ');
-       result += `You can preview all these themes here: ${packageJson.repository.url}/tree/master/docs/themes`;
+        return result;
+      }
 
-       return result;
-     }
+      case 'set': {
+        if (args.length !== 2) {
+          return usage;
+        }
 
-     case 'set': {
-       if (args.length !== 2) {
-         return usage;
-       }
+        const selectedTheme = args[1];
+        const t = themes.find((t) => t.name.toLowerCase() === selectedTheme);
 
-       const selectedTheme = args[1];
-       const t = themes.find((t) => t.name.toLowerCase() === selectedTheme);
+        if (!t) {
+          return `Theme '${selectedTheme}' not found. Try 'theme ls' to see all available themes.`;
+        }
 
-       if (!t) {
-         return `Theme '${selectedTheme}' not found. Try 'theme ls' to see all available themes.`;
-       }
+        theme.set(t);
 
-       theme.set(t);
+        return `Theme set to ${selectedTheme}`;
+      }
 
-       return `Theme set to ${selectedTheme}`;
-     }
+      default: {
+        return usage;
+      }
+    }
+  },
+  repo: () => {
+    window.open(packageJson.repository.url, '_blank');
 
-     default: {
-       return usage;
-     }
-   }
- },
+    return 'Opening repository...';
+  },
+  clear: () => {
+    history.set([]);
 
- repo: () => {
-   window.open(packageJson.repository.url, '_blank');
+    return '';
+  },
+  email: () => {
+    window.open(`mailto:${packageJson.author.email}`);
 
-   return 'Opening repository...';
- },
- clear: () => {
-   history.set([]);
+    return `Opening mailto:${packageJson.author.email}...`;
+  },
+  donate: () => {
+    window.open(packageJson.funding.url, '_blank');
 
-   return '';
- },
- email: () => {
-   window.open(`mailto:${packageJson.author.email}`);
+    return 'Opening donation url...';
+  },
+  weather: async (args: string[]) => {
+    const city = args.join('+');
 
-   return `Opening mailto:${packageJson.author.email}...`;
- },
- donate: () => {
-   window.open(packageJson.funding.url, '_blank');
+    if (!city) {
+      return 'Usage: weather [city]. Example: weather Brussels';
+    }
 
-   return 'Opening donation url...';
- },
- weather: async (args: string[]) => {
-   const city = args.join('+');
+    const weather = await fetch(`https://wttr.in/${city}?ATm`);
 
-   if (!city) {
-     return 'Usage: weather [city]. Example: weather Chicago';
-   }
+    return weather.text();
+  },
+  exit: () => {
+    return 'Please close the tab to exit.';
+  },
+  curl: async (args: string[]) => {
+    if (args.length === 0) {
+      return 'curl: no URL provided';
+    }
 
-   const weather = await fetch(`https://wttr.in/${city}?ATm`);
+    const url = args[0];
 
-   return weather.text();
- },
- exit: () => {
-   return 'Please close the tab to exit.';
- },
- curl: async (args: string[]) => {
-   if (args.length === 0) {
-     return 'curl: no URL provided';
-   }
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
 
-   const url = args[0];
-
-   try {
-     const response = await fetch(url);
-     const data = await response.text();
-
-     return data;
-   } catch (error) {
-     return `curl: could not fetch URL ${url}. Details: ${error}`;
-   }
- },
- banner: () => `
+      return data;
+    } catch (error) {
+      return `curl: could not fetch URL ${url}. Details: ${error}`;
+    }
+  },
+  banner: () => `
 ╔═══╗╔═══╗╔═══╗╔╗╔═╗╔╗╔╗╔╗╔══╗╔═╗─╔╗╔═══╗╔═══╗
 ╚╗╔╗║║╔═╗║║╔═╗║║║║╔╝║║║║║║╚╣║╝║║╚╗║║╚╗╔╗║║╔═╗║
 ─║║║║║║─║║║╚═╝║║╚╝╝─║╚╝╚╝║─║║─║╔╗╚╝║─║║║║║╚══╗
@@ -137,22 +124,4 @@ v${packageJson.version}
 
 Type 'help' to see list of available commands.
 `,
-invite: async () => {
-    // Ask the user for their invite code
-    const inviteCode = prompt('Enter your invite code:');
-    
-    // Check if invite code is provided
-    if (!inviteCode) {
-        return 'Invite code is required.';
-    }
-    
-    // Construct the URL with the invite code
-    const inviteURL = `https://invite.darkwinds.net/j/${inviteCode}`;
-    
-    // Open the URL in a new tab
-    window.open(inviteURL, '_blank');
-    
-    // Return a confirmation message
-    return `Opening invite link with code ${inviteCode}...`;
-}
 };
